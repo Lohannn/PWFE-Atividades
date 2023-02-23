d59d0b2f7634f3e7e31d3c1cdc7b0af6b0e8e375
Lohannes da Silva Costa <lohannes.s26@gmail.com>
2023-02-23 16:10:06 -0300

    fim

---
+++ Array/js/main.js
@@ -0,0 +1,37 @@
+'use strict'
+
+import { produtos } from './produtos.js'
+
+const criaCard = ( produto ) => {
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    const photo = document.createElement('img')
+    photo.classList.add('card__image')
+    photo.src = `./img/${produto.image}`
+
+    const title = document.createElement('h5')
+    title.classList.add('card__title')
+    title.textContent = produto.name
+
+    const description = document.createElement('p')
+    description.classList.add('card__description')
+    description.textContent = produto.description
+
+    const price = document.createElement('span')
+    price.classList.add('card__price')
+    price.textContent = produto.price
+
+    card.append(photo, title, description, price)
+
+    return card
+}
+
+const carregarProdutos = () => {
+    const container = document.getElementById('container')
+    const cards = produtos.map( criaCard )
+    
+    container.replaceChildren(...cards)
+}
+
+carregarProdutos()
